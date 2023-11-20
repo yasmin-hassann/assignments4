2657e0e1ef7241fe42aff5ac0b8407fe4dea6d21
Yasmin Hassan <yhhgzb@mail.umkc.edu>
2023-11-19 19:32:31 -0600

    final

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -12,6 +12,161 @@ public class Board extends Group {
 
     private int checked_count = 0;
 
+    Board(int size, Game game) {
+        board = new BoardLabel[size][size];
+        this.size = size;
+        this.game = game;
+
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                board[i][j] = new BoardLabel(i, j);
+                board[i][j].setStyle("-fx-border-color: #000000; -fx-border-width: 1px; -fx-background-color: #ffffff; -fx-font-size: 24px; -fx-font-weight: bold; -fx-alignment: center; -fx-padding: 10px;");
+                board[i][j].setPrefSize(50, 50);
+                board[i][j].setTranslateX(50 * j);
+                board[i][j].setTranslateY(50 * i);
+                board[i][j].setOnMouseClicked(e -> clickLable((BoardLabel) e.getSource()));
+                getChildren().add(board[i][j]);
+            }
+        }
+    }
+
+    public void clickLable(BoardLabel label) {
+
+        if (label.getText().equals("S") || label.getText().equals("O")) {
+            return;
+        }
+
+        game.updateMode();
+
+        if (game.getIsSSelected()) {
+            label.setText("S");
+        } else {
+            label.setText("O");
+        }
+
+        checkSOS(label);
+        checked_count++;
+        game.alterTurn();
+
+    }
+
+    private void checkSOS(BoardLabel label) {
+
+        int i = label.getI();
+        int j = label.getJ();
+
+
+        if (label.getText().equals("S")) {
+
+            for (int k = -1; k <= 1; k++) {
+                for (int l = -1; l <= 1; l++) {
+                    if (k == 0 && l == 0) {
+                        continue;
+                    }
+
+                    if (isLabelO(i + k, j + l) && isLabelS(i + 2 * k, j + 2 * l)) {
+                        addLine(i, j, i + 2 * k, j + 2 * l);
+                        game.incrementScore();
+                    }
+                }
+            }
+        } else if (label.getText().equals("O")) {
+
+            for (int k = -1; k <= 1; k++) {
+                for (int l = -1; l <= 1; l++) {
+                    if (k == 0 && l == 0) {
+                        continue;
+                    }
+
+                    if (isLabelS(i + k, j + l) && isLabelS(i - k, j - l)) {
+                        addLine(i + k, j + l, i - k, j - l);
+                        game.incrementScore();
+                    }
+                }
+            }
+
+        }
+
+    }
+
+    public boolean isGameOver() {
+        return checked_count == size * size;
+    }
+
+    private void addLine(int i, int j, int i1, int i2) {
+        Line line = new Line();
+
+        Label label1 = board[i][j];
+        Label label2 = board[i1][i2];
+
+        line.setStartX(label1.getTranslateX() + 25);
+        line.setStartY(label1.getTranslateY() + 25);
+
+        line.setEndX(label2.getTranslateX() + 25);
+        line.setEndY(label2.getTranslateY() + 25);
+
+        if (game.isBluePlayerTurn())
+            line.setStroke(Color.BLUE);
+        else
+            line.setStroke(Color.RED);
+
+        getChildren().add(line);
+    }
+
+    private boolean isLabelO(int i, int j) {
+        if (i < 0 || i >= size || j < 0 || j >= size) {
+            return false;
+        }
+
+        return board[i][j].getText().equals("O");
+    }
+
+    private boolean isLabelS(int i, int j) {
+        if (i < 0 || i >= size || j < 0 || j >= size) {
+            return false;
+        }
+
+        return board[i][j].getText().equals("S");
+    }
+
+
+    char[][] getBoardState() {
+        char[][] boardStates = new char[getBoardSize()][getBoardSize()];
+
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++) {
+                String s = board[i][j].getText();
+
+                if (s.length() > 0)
+                    boardStates[i][j] = s.charAt(0);
+                else
+                    boardStates[i][j] = 'x';
+            }
+        return boardStates;
+    }
+
+    BoardLabel getBoardLabel(int row, int col) {
+        if (row < 0 || row >= size || col < 0 || col >= size) {
+            return null;
+        }
+
+        return board[row][col];
+
+    }
+
+
+    public int getBoardSize() {
+        return size;
+    }
+
+    /*
+    private final Game game;
+    private final int size;
+    private final BoardLabel[][] board;
+
+    private int checked_count = 0;
+
 
     public Board(int size, Game game) {
         board = new BoardLabel[size][size];
@@ -164,7 +319,7 @@ public class Board extends Group {
         return checked_count == size * size;
     }
 
-
+*/
 }
 
 
--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -139,6 +139,232 @@ public class Game extends Scene {
         }
     }
 
+    public void alterTurn() {
+
+        if (board.isGameOver() || (isSimpleMode && (blue.getScore() > 0 || red.getScore() > 0))) {
+            gameOver();
+            return;
+        }
+        isBluePlayerTurn = !isBluePlayerTurn;
+        if (isBluePlayerTurn) {
+            statusLabel.setText("Blue Player's Turn");
+            isSSelected = blue.isModeS();
+            blue.setActivatedBackground();
+            red.resetBackground();
+
+            if (blue.isComputerSelected()) {
+                blue.playNextMove(board);
+            }
+        } else {
+            statusLabel.setText("Red Player's Turn");
+            isSSelected = red.isModeS();
+            red.setActivatedBackground();
+            blue.resetBackground();
+
+            if (red.isComputerSelected())
+                red.playNextMove(board);
+        }
+    }
+
+    private void gameOver() {
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Game Over");
+        alert.setHeaderText("Game Over");
+        if (blue.getScore() > red.getScore()) {
+            alert.setContentText("Blue Player Wins!");
+        } else if (blue.getScore() < red.getScore()) {
+            alert.setContentText("Red Player Wins!");
+        } else {
+            alert.setContentText("Draw!");
+        }
+
+        alert.showAndWait();
+        resetGame();
+
+    }
+
+    private void resetGame() {
+        int boardSize = takeGameInfo();
+
+        board = new Board(boardSize, this);
+        SOS.setText("SOS (" + (isSimpleMode ? "Simple" : "General") + " Mode)                            ");
+
+        blue.resetScore();
+        red.resetScore();
+        pane.setCenter(board);
+
+        isSSelected = true;
+        statusLabel.setText("Blue Player's Turn");
+        isSSelected = blue.isModeS();
+        blue.setActivatedBackground();
+        red.resetBackground();
+
+        Window window = getRoot().getScene().getWindow();
+
+        int width = boardSize * 60 + 300;
+        int height = boardSize * 50 + 200;
+
+        if (width < 600) width = 600;
+        if (height < 300) height = 300;
+        window.setWidth(width);
+        window.setHeight(height);
+    }
+
+
+    public void incrementScore() {
+        if (isBluePlayerTurn) {
+            blue.incrementScore();
+        } else {
+            red.incrementScore();
+        }
+    }
+
+    public boolean isBluePlayerTurn() {
+        return isBluePlayerTurn;
+    }
+}
+
+/*
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Window;
+
+public class Game extends Scene {
+
+    private final BorderPane pane;
+    //keep track of the player's choice (S or O)
+    private Boolean isSSelected = true; //true = S, false = O
+    //Two players
+    private final UserPanel blue;
+    private final UserPanel red;
+
+    private Boolean isBluePlayerTurn = true; //true = blue, false = red
+
+    private boolean isSimpleMode = true; //true = simple, false = general
+
+    private final Label statusLabel = new Label("Blue Player's Turn");
+    private final Label SOS;
+
+    private Board board;
+
+
+    public Game() {
+        super(new BorderPane());
+        int boardSize = takeGameInfo();
+        pane = (BorderPane) getRoot();
+        blue = new UserPanel("Blue Player", this);
+        red = new UserPanel("Red Player", this);
+
+        //build the top panel
+        HBox topPanel = new HBox();
+        topPanel.setStyle("-fx-border-color: #000000; -fx-padding: 10px;");
+        topPanel.prefHeight(60);
+
+        SOS = new Label("SOS (" + (isSimpleMode ? "Simple" : "General") + " Mode)                            ");
+        SOS.setStyle("-fx-font-size: 24px; -fx-font-weight: bold;");
+
+        topPanel.getChildren().add(SOS);
+
+
+        Button reset = new Button("Reset");
+        reset.setOnAction(e -> {
+            resetGame();
+        });
+        topPanel.getChildren().add(reset);
+        pane.setTop(topPanel);
+
+        //build the left panel
+        pane.setLeft(blue);
+        pane.setRight(red);
+
+        FlowPane bottom = new FlowPane();
+        bottom.setStyle("-fx-border-color: #000000; -fx-padding: 10px;");
+        bottom.prefHeight(60);
+        bottom.getChildren().add(statusLabel);
+        pane.setBottom(bottom);
+
+        board = new Board(boardSize, this);
+        pane.setCenter(board);
+
+        blue.setActivatedBackground();
+    }
+
+    private int takeGameInfo() {
+        //ask the user for the game mode
+        Dialog dialog = new Dialog<>();
+        dialog.setTitle("Game Mode");
+        dialog.setHeaderText("Choose the game mode");
+
+        //hide the alert confirmation button
+
+        Window window = dialog.getDialogPane().getScene().getWindow();
+        window.setOnCloseRequest(event -> window.hide());
+
+        DialogPane dialogPane = dialog.getDialogPane();
+
+        Button simple = new Button("Simple");
+        simple.setOnAction(e -> {
+            isSimpleMode = true;
+            System.out.println("simple");
+            dialog.close();
+            dialog.hide();
+            window.hide();
+        });
+
+        Button general = new Button("General");
+        general.setOnAction(e -> {
+            isSimpleMode = false;
+            System.out.println("general");
+            dialog.close();
+            dialog.hide();
+            window.hide();
+        });
+
+        HBox hbox = new HBox();
+        hbox.setSpacing(65);
+        hbox.getChildren().addAll(simple, general);
+        dialogPane.setContent(hbox);
+        dialog.showAndWait();
+
+
+        //ask for board size
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Board Size");
+        alert.setHeaderText("Choose the board size");
+
+        ChoiceBox<Integer> choiceBox = new ChoiceBox<>();
+        choiceBox.getItems().addAll(3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+        choiceBox.setValue(8);
+        //hide the alert cancel button
+        Button cancelButton = (Button) alert.getDialogPane().lookupButton(ButtonType.CANCEL);
+        cancelButton.setVisible(false);
+
+        alert.getDialogPane().setContent(choiceBox);
+        alert.showAndWait();
+
+
+        return choiceBox.getValue();
+
+    }
+
+
+    //get type of game mode for current player
+    public boolean getIsSSelected() {
+        return isSSelected;
+    }
+
+    public void updateMode() {
+        if (isBluePlayerTurn) {
+            isSSelected = blue.isModeS();
+        } else {
+            isSSelected = red.isModeS();
+        }
+    }
+
 
 
 public void alterTurn() {
@@ -235,3 +461,5 @@ public void alterTurn() {
         return isBluePlayerTurn;
     }
 }
+
+ */
